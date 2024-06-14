c87718de8af31b1159b4c982fdf7683b5cf82944
Nicholas Carnevale <73004597+nickcarnevale@users.noreply.github.com>
2024-06-14 08:47:57 -0400

    add src

---
+++ src/variables/variables3.rs
@@ -0,0 +1,51 @@
+// variables3.rs
+// Make me compile! Scroll down for hints :)
+
+#[test]
+fn main() {
+    //can either use shadowing or mutability
+    
+    let mut x = 3;
+    println!("Number {}", x);
+    x = 5;
+    //or get rid or the mut, and add
+    //let x = 5
+
+    println!("Number {}", x);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// In Rust, variable bindings are immutable by default. But here we're trying
+// to reassign a different value to x! There's a keyword we can use to make
+// a variable binding mutable instead.
